f87093eddf6d3749ef7255d476b8781febebda0d
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2019-01-22 10:06:27 +0600

    Initialize

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!usr/bin/python
+from setuptools import setup , find_packages
+setup (
+	name = 'git-miner',
+	description = 'Cli to show all the git status',
+	version = '0.10',
+	packages = find_packages(), # list of all packages
+    	install_requires = ['pygit2', 'colorama'],
+    	python_requires='>=2.7', # any python greater than 2.7
+	test_suite="tests", # where to find tests
+	entry_points = {
+		'console_scripts': [
+			'git-stat = git-stat.__main__:main'
+			]
+		}
+	)
