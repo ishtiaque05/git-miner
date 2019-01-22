49a5a9d73d8fbf5194a10a60422ba6638d7a865a
Syed Ishtiaque Ahmad <ishtiaque05@gmail.com>
2019-01-22 14:47:45 +0600

    Find top n contributors in the project

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 #!usr/bin/python
 from setuptools import setup , find_packages
 setup (
-	name = 'git-miner',
-	description = 'Cli to show all the git status',
+	name = 'GitMiner',
+	description = 'An application getting Git stats',
 	version = '0.10',
 	packages = find_packages(), # list of all packages
-    	install_requires =  ['colorama'],
-    	python_requires='>=2.7', # any python greater than 2.7
+    	install_requires = ['colorama'],
+    	python_requires='>=3.6', # any python greater than 2.7
 	test_suite="tests", # where to find tests
 	entry_points = {
 		'console_scripts': [
-			'git-stat = git-stat.__main__:main'
+			'gitstat=gitstat.__main__:main'
 			]
 		}
 	)
+
